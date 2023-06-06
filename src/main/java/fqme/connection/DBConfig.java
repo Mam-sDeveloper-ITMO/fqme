83f2df271b651ef18a9921986d579b5f129ddddb
butvinm <butvin.mihail@yandex.ru>
2023-06-07 01:45:11 +0300

    Impl method for loading DBConfig from config file

--- src/main/java/fqme/connection/DBConfig.java
+++ src/main/java/fqme/connection/DBConfig.java
@@ -1,7 +1,11 @@
 package fqme.connection;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
+import lombok.Cleanup;
 import lombok.Data;
 
 /**
@@ -23,4 +27,23 @@ public class DBConfig {
      * Password for database connection.
      */
     private final String password;
+
+    /**
+     * Create a new DBConfig instance from a configuration file.
+     *
+     * @param configPath path to the configuration file
+     * @return a new DBConfig instance
+     */
+    public static DBConfig fromConfigFile(String configPath) throws IOException, FileNotFoundException {
+        Properties properties = new Properties();
+
+        @Cleanup
+        FileInputStream inputStream = new FileInputStream(configPath);
+        properties.load(inputStream);
+
+        return new DBConfig(
+                properties.getProperty("url"),
+                properties.getProperty("username"),
+                properties.getProperty("password"));
+    }
 }
