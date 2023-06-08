f7a8c2261ba1529328fc45888325ece5f5e5e125
butvinm <butvin.mihail@yandex.ru>
2023-06-08 11:26:11 +0300

    Add manual tests

--- src/test/java/fqme/integration/ViewTest.java
+++ src/test/java/fqme/integration/ViewTest.java
@@ -1,20 +1,61 @@
 package fqme.integration;
 
-import java.util.Optional;
+import java.sql.Connection;
+import java.util.Random;
+import java.util.Set;
 
+import fqme.connection.ConnectionManager;
 import fqme.connection.DBConfig;
 import fqme.model.Model;
+import fqme.utils.LocationModel;
 import fqme.utils.TestModel;
 import fqme.view.View;
 
 public class ViewTest {
     public static void main(String[] args) throws Exception {
-        DBConfig dbConfig = new DBConfig("jdbc:postgresql://localhost:5432/postgres", "postgres", "160205");
+        Random random = new Random();
 
-        Model.register(TestModel.class, dbConfig);
+        // prepare
+        Model.register(TestModel.class);
+        Model.register(LocationModel.class);
 
-        View<TestModel> view = View.of(TestModel.class);
+        DBConfig config = DBConfig.fromConfigFile("./db.cfg");
+        ConnectionManager.bind(TestModel.class, config);
 
-        Optional<TestModel> model = view.get(1);
+        // usage
+        Connection connection = ConnectionManager.getConnection(TestModel.class);
+
+        View<LocationModel> locationView = View.of(LocationModel.class, connection);
+        View<TestModel> view = View.of(TestModel.class, connection);
+
+        for (int i = 0; i < 1000; i++) {
+            LocationModel location = new LocationModel("New York", random.nextInt(20) - 20, random.nextInt(20) - 20);
+            location = locationView.put(location).iterator().next();
+
+            TestModel model = new TestModel(10, "test", location.getId());
+
+            LocationModel location2 = new LocationModel("Mexico", random.nextInt(20) - 20, random.nextInt(20) - 20);
+            location2 = locationView.put(location2).iterator().next();
+            
+            TestModel model2 = new TestModel("Alex", location2.getId());
+
+            Set<TestModel> result = view.put(model);
+            result = view.put(model);
+
+            result = view.put(model2);
+
+            TestModel model3 = view.get(TestModel.id_.eq(10)).iterator().next();
+            // assert model3.equals(model);
+
+            Set<TestModel> result2 = view.get(TestModel.name_.eq("Alex"));
+            // assert result2.size() == 1;
+            Set<TestModel> result3 = view.get(TestModel.name_.eq("Alex").and(TestModel.id_.eq(10)));
+
+            Set<TestModel> result4 = view.get(LocationModel.id_.eq(model3.getLocationId()));
+
+            view.delete(TestModel.id_.eq(10));
+
+            System.out.println(result);
+        }
     }
 }
